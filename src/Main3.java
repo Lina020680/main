75e479f32d6af6b71753d23d16c7138698634509
lina <lina.parts@googlemail.com>
2023-11-24 10:03:38 +0100

    hello

---
+++ src/Main3.java
@@ -0,0 +1,22 @@
+public class Main3 {
+
+
+    public static void main(String[] args) {
+     int a;
+     int x = 1;
+        System.out.println(x);
+
+        if (x == 10){
+            int y = 30;
+            System.out.println("x= " + x + " , y=" + y);
+        }
+
+    }
+
+    public static void method1(){
+        int a = 2;
+        int x = 1;
+        System.out.println(x++);
+        System.out.println(a);
+    }
+}
